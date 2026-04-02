2e24b0728885c7024cc2efd98f6a41ea96b5dde1
Agina_Master <your_email@example.com>
2026-04-03 01:36:05 +0200

    Socket Lab 100% Completed

--- client.c
+++ client.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 int main() {
-    int server_fd, client_fd;
-    struct sockaddr_in addr;
-    char buffer[1024] = {0};
+    int sock;
+    struct sockaddr_in server;
+    char message[1024], server_reply[2048];
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(4444);
 
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(4444);
-
-    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
-    listen(server_fd, 1);
-
-    printf("Server listening on port 4444...\n");
-
-    client_fd = accept(server_fd, NULL, NULL);
-
-    while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        read(client_fd, buffer, 1024);
-
-        printf("Received: %s\n", buffer);
-
-        if (strncmp(buffer, "scan", 4) == 0) {
-            send(client_fd, "Open port: 22\n", 14, 0);
-        }
-        else if (strncmp(buffer, "getflag", 7) == 0) {
-            send(client_fd, "FLAG{real_lab_flag}\n", 21, 0);
-        }
-        else {
-            send(client_fd, "Unknown command\n", 17, 0);
-        }
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Connect failed. Is the server running?\n");
+        return 1;
     }
 
+    printf("Connected to Lab Server. Type 'help' for commands.\n");
+
+    while(1) {
+        printf(">> ");
+        // استخدام fgets بدل scanf لقراءة الأوامر المركبة
+        fgets(message, sizeof(message), stdin);
+        message[strcspn(message, "\n")] = 0; // مسح السطر الجديد
+
+        if (send(sock, message, strlen(message), 0) < 0) break;
+        
+        memset(server_reply, 0, sizeof(server_reply));
+        if (recv(sock, server_reply, sizeof(server_reply), 0) <= 0) break;
+        
+        printf("%s\n", server_reply);
+    }
+    close(sock);
     return 0;
 }
--- server.c
+++ server.c
@@ -7,37 +7,69 @@
 int main() {
     int server_fd, client_fd;
     struct sockaddr_in addr;
-    char buffer[1024] = {0};
+    char buffer[1024];
 
+    // إنشاء Socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd == -1) {
+        perror("Socket failed");
+        return 1;
+    }
 
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(4444);
 
-    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
-    listen(server_fd, 1);
+    // ربط البورت
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("Bind failed");
+        return 1;
+    }
+
+    // الاستماع
+    if (listen(server_fd, 1) < 0) {
+        perror("Listen failed");
+        return 1;
+    }
 
     printf("Server listening on port 4444...\n");
 
     client_fd = accept(server_fd, NULL, NULL);
 
+    printf("Client connected!\n");
+
     while (1) {
         memset(buffer, 0, sizeof(buffer));
-        read(client_fd, buffer, 1024);
+
+        int bytes = read(client_fd, buffer, sizeof(buffer));
+        if (bytes <= 0) {
+            printf("Client disconnected\n");
+            break;
+        }
 
         printf("Received: %s\n", buffer);
 
+        // أوامر اللعبة
         if (strncmp(buffer, "scan", 4) == 0) {
             send(client_fd, "Open port: 22\n", 14, 0);
         }
+        else if (strncmp(buffer, "connect", 7) == 0) {
+            send(client_fd, "Connecting...\nSuccess! System accessed.\n", 41, 0);
+        }
         else if (strncmp(buffer, "getflag", 7) == 0) {
-            send(client_fd, "FLAG{real_lab_flag}\n", 21, 0);
+            send(client_fd, "FLAG{real_lab_mode}\n", 20, 0);
+        }
+        else if (strncmp(buffer, "help", 4) == 0) {
+            send(client_fd, "Commands: scan | connect | getflag | help\n", 44, 0);
         }
         else {
             send(client_fd, "Unknown command\n", 17, 0);
         }
     }
+
+    close(client_fd);
+    close(server_fd);
 
     return 0;
 }
